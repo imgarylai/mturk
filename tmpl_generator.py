7dd8b9315afeddd33136c941486509ee798b99e1
Gary Lai <garylai1990@gmail.com>
2017-07-27 11:55:21 -0400

    update tmpl generator

--- tmpl_generator.py
+++ tmpl_generator.py
@@ -3,14 +3,13 @@ import jinja2
 import ujson
 import htmlmin
 import os.path
-
+import csv
 
 def render(tpl_path, thread):
     path, filename = os.path.split(tpl_path)
     return jinja2.Environment(
         loader=jinja2.FileSystemLoader(path or './')
     ).get_template(filename).render(thread=thread)
-
 
 def html_question(thread):
     for email in thread['emails']:
@@ -26,49 +25,24 @@ def html_question(thread):
 
 
 if __name__ == '__main__':
-    with open('classified/0.4.json') as inf:
-        data = ujson.load(inf)
 
-    # max_col_nums = max(
-    #     list(map(
-    #         lambda thread: sum(
-    #             list(map(
-    #                 lambda email: sum(list(map(
-    #                     lambda ent: ent[1] or ent[2] == 'PERSON' or ent[2] == 'ORG', email['ents']
-    #                 ))), thread['emails'])
-    #             )
-    #         ), data)
-    #     )
-    # )
+    with open('classified/dir.csv', 'r') as f:
+        reader = csv.reader(f)
+        dir_list = list(reader)
 
     col = ['question_html']
 
-    # for i in range(max_col_nums):
-    #     col.append("question_{}".format(i + 1))
+    for dir in dir_list[0]:
+        json = "{}/{}.json".format(dir, os.path.basename(os.path.normpath(dir)))
+        file = "{}/{}.csv".format(dir, os.path.basename(os.path.normpath(dir)))
+        with open(json) as inf:
+            data = ujson.load(inf)
 
-    print(col)
-    df = pd.DataFrame(columns=col)
-    # print(df)
+        df = pd.DataFrame(columns=col)
 
-    for thread in data:
-        minified = htmlmin.minify(html_question(thread), remove_empty_space=True)
-    #     ents = []
-    #     for email in thread['emails']:
-    #         for ent in email['ents']:
-    #             if ent[1] or ent[2] == 'PERSON' or ent[2] == 'ORG':
-    #                 ents.append(ent[0])
-    #
-    #     # print(ents)
-    #     ents += ["DO NOT ANSWER"] * (max_col_nums - len(ents))
-    #     # print(ents)
-    #     row = [minified] + ents
-    #     # print(row)
-    #     mturk_df = pd.DataFrame([row],
-    #                             columns=col)
-    #     df = df.append(mturk_df, ignore_index=True)
+        for thread in data:
+            minified = htmlmin.minify(html_question(thread), remove_empty_space=True)
+            mturk_df = pd.DataFrame([[minified]], columns=col)
+            df = df.append(mturk_df, ignore_index=True)
 
-        mturk_df = pd.DataFrame([[minified]],
-                                columns=col)
-        df = df.append(mturk_df, ignore_index=True)
-
-    df.to_csv('0.4.csv', index=False)
+        df.to_csv(file, index=False)
