854299cac232713b66bd2294a129b8d7012750eb
Gary Lai <garylai1990@gmail.com>
2017-07-27 11:41:05 -0400

    classifier.py

--- classifier.py
+++ classifier.py
@@ -1,4 +1,5 @@
 import ujson
+import csv
 import os.path
 
 with open("email.threads.strict.only.ents.json") as f:
@@ -6,19 +7,26 @@ with open("email.threads.strict.only.ents.json") as f:
 
 
 def cal_reward(l):
-    read_instruction = 3.0
+    read_instruction = 1.0
     word_count = l * 1 / 30.0
-    return round((read_instruction + word_count) / 60.0 * 6.0, 2)
+    return round((read_instruction + word_count) / 60.0 * 6.0, 3)
 
 
 def one_five_round(x):
-    return round(x * 5) / 5
+    return round(x * 10) / 10
 
 
 if __name__ == '__main__':
+    dir_list = []
+    prefix = 'classified'
     for thread in data:
-        file = "{}.json".format(
-            one_five_round(cal_reward(sum(list(map(lambda email: len(email['ents']), thread['emails']))))))
+        file_name = str(one_five_round(cal_reward(sum(list(map(lambda email: len(email['ents']), thread['emails']))))))
+        dir = "{0}/{1}".format(prefix, file_name)
+        file = "{0}/{1}.json".format(dir, file_name)
+
+        if not os.path.isdir(dir):
+            dir_list.append(dir)
+            os.mkdir(dir)
 
         if not os.path.exists(file):
             a = []
@@ -30,5 +38,8 @@ if __name__ == '__main__':
 
         tmp.append(thread)
 
-        with open(file, 'w') as outf:
-            ujson.dump(tmp, outf)
+        with open(file, 'w') as out:
+            ujson.dump(tmp, out, indent=2)
+    with open("{0}/dir.csv".format(prefix), "w") as f:
+        writer = csv.writer(f)
+        writer.writerow(dir_list)
